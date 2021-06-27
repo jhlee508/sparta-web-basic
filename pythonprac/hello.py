eec218bcb68a13d04295179590991f8cc1005019
chris <chrisjae508@gmail.com>
2021-06-28 00:32:52 +0900

    crawling

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,1 +1,13 @@
-print('hello sparta')
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303', headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 코딩 시작
+title = soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
+
+print(title.get_text())
